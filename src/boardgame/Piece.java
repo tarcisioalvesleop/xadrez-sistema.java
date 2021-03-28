84fb772f956a9ef5acba65f9d439d56e2e55b82c
Tarcisio Alves Bertolino <tarcisioalvesleop@hotmail.com>
2021-03-28 20:08:52 -0300

    Starting implement class Board and Piece.

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,20 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		//position = null;
+	}
+	
+	protected Board getBoard() { 
+		//somente class detntro do mesmo pacote ou sub classes terao acesso
+		return board;
+	}
+	
+	
 
 }
